9baa9864722af8d2a935a5b33ac731d6344e8363
parkhongbeen <pack122@naver.com>
2020-04-14 20:54:50 +0900

    model구현, django-extensions추가, config/settings설정

--- app/config/settings.py
+++ app/config/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'studyquery.apps.StudyqueryConfig',
+
 ]
 
 MIDDLEWARE = [
