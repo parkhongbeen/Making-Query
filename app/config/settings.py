b31e1e3e2011ac8d87153e1dc0da9cfbb27d079f
parkhongbeen <pack122@naver.com>
2020-04-15 17:01:27 +0900

    data가 없어서 마무리 함

--- app/config/settings.py
+++ app/config/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Blog.apps.BlogConfig'
 
-    'studyquery.apps.StudyqueryConfig',
 
 ]
 
